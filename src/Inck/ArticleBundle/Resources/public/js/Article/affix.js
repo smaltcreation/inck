de9a3eaf3cf4849f0dcfa04ef7b96c955d7a8a54
Samy Laumonier <159024@supinfo.com>
2014-09-10 23:41:25 +0200

    Affix désactivé après une redimension #47

--- src/Inck/ArticleBundle/Resources/public/js/Article/affix.js
+++ src/Inck/ArticleBundle/Resources/public/js/Article/affix.js
@@ -1,50 +1,66 @@
 $(document).ready(function(){
-    /*
-     * Clamped-width.
-     * Usage:
-     *  <div data-clampedwidth=".myParent">This long content will force clamped width</div>
-     *
-     * Author: LV
-     */
-    $('[data-clampedwidth]').each(function () {
-        var elem = $(this);
-        //elem.attr('data-offset-bottom', $('article').height());
-        var parentPanel = elem.data('clampedwidth');
-        var resizeFn = function () {
-            var sideBarNavWidth = $(parentPanel).width() - parseInt(elem.css('paddingLeft')) - parseInt(elem.css('paddingRight')) - parseInt(elem.css('marginLeft')) - parseInt(elem.css('marginRight')) - parseInt(elem.css('borderLeftWidth')) - parseInt(elem.css('borderRightWidth'));
-            elem.css('width', sideBarNavWidth);
-        };
+    clampedWidth();
 
-        resizeFn();
-        $(window).resize(resizeFn);
-    });
+    function clampedWidth(){
+        /*
+         * Clamped-width.
+         * Usage:
+         *  <div data-clampedwidth=".myParent">This long content will force clamped width</div>
+         *
+         * Author: LV
+         */
+        $('[data-clampedwidth]').each(function () {
+            var elem = $(this);
+            //elem.attr('data-offset-bottom', $('article').height());
+            var parentPanel = elem.data('clampedwidth');
+            var resizeFn = function () {
+                var sideBarNavWidth = $(parentPanel).width() - parseInt(elem.css('paddingLeft')) - parseInt(elem.css('paddingRight')) - parseInt(elem.css('marginLeft')) - parseInt(elem.css('marginRight')) - parseInt(elem.css('borderLeftWidth')) - parseInt(elem.css('borderRightWidth'));
+                elem.css('width', sideBarNavWidth);
+            };
+
+            resizeFn();
+            $(window).resize(resizeFn);
+        });
+    }
 
     // Affix
     var social = $('#article-social');
-    var offset = null;
+    setAffix();
 
-    social.affix({
-        offset: {
-            top: function(){
-                return $('#header').outerHeight() - 50;
-            },
-            bottom: function(){
-                return $('#footer').find('footer:first').outerHeight() + 100;
-            }
-        }
-    }).on('affix.bs.affix', function(){
-        if(offset == null){
-            offset = social.offset().left;
-        }
-    }).on('affixed.bs.affix affixed-top.bs.affix affixed-bottom.bs.affix', function(){
-        social.offset({
-            left: offset
-        });
-    });
+    function setAffix(){
+        var offset = null;
+
+        social
+            .affix({
+                offset: {
+                    top: function(){
+                        return $('#header').outerHeight() - 50;
+                    },
+                    bottom: function(){
+                        return $('#footer').find('footer:first').outerHeight() + 100;
+                    }
+                }
+            })
+            .on('affix.bs.affix', function(){
+                if(offset === null){
+                    offset = social.offset().left;
+                }
+            }).on('affixed.bs.affix affixed-top.bs.affix affixed-bottom.bs.affix', function(){
+                social.offset({
+                    left: offset
+                });
+            })
+        ;
+    }
 
     // Resize
     function windowResized(){
-        offset = social.offset().left;
+        $(window).off('.affix');
+        social
+            .removeData('bs.affix')
+            .removeClass('affix affix-top affix-bottom')
+            .removeAttr('style');
+        clampedWidth();
     }
 
     var resizing;
