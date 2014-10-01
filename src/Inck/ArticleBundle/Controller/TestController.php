7c44ebd4131512a7b1123daa9d6877d1d2cdc3c6
Samy Laumonier <159024@supinfo.com>
2014-10-02 00:49:32 +0200

    Création d'un test pour le findByFilters #75

---
+++ src/Inck/ArticleBundle/Controller/TestController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Inck\ArticleBundle\Controller;
+
+use Inck\ArticleBundle\Entity\ArticleRepository;
+use Inck\UserBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/test")
+ */
+class TestController extends Controller
+{
+    /**
+     * @Route("/user/{id}")
+     * @Template()
+     * @ParamConverter("user", class="InckUserBundle:User")
+     * @param User $user
+     * @return array
+     */
+    public function indexAction(User $user)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('InckArticleBundle:Article');
+
+        /** @var ArticleRepository $articles */
+        $articles = $repository->findByFilters(array(
+            'type'      => 'published',
+            'author'    => $user,
+        ), false);
+
+        return array(
+            'user'      => $user,
+            'articles'  => $articles,
+        );
+    }
+}
