8434288c98d9a3045a2e1f74b8a0649dbe215d73
dridzou <lucivel_v@hotmail.com>
2014-10-08 12:36:43 +0200

    Modification du menu #81  - Mise en place de la progress bar dans Article:show.html.twig

--- src/Inck/ArticleBundle/Resources/public/js/Article/show.js
+++ src/Inck/ArticleBundle/Resources/public/js/Article/show.js
@@ -1,11 +1,13 @@
 $(function() {
     $(window).scroll(function(){
         var distanceTop = $('#last').offset().top - $(window).height();
-
         if  ($(window).scrollTop() > distanceTop)
             $('#slidebox').animate({'right':'0px'},300);
         else
-        $('#slidebox').stop(true).animate({'right':'-430px'},100);
+            $('#slidebox').stop(true).animate({'right':'-430px'},100);
+        var distanceProgress = ($('#last').offset().top - $(window).height());
+        var articleProgressWidth = ($('#article-progress-bar').offset().top / distanceProgress);
+        document.getElementById('article-progress-bar').style.width=(articleProgressWidth*100)+"%";
 });
 
 $('.close-slidebox').bind('click',function(){
