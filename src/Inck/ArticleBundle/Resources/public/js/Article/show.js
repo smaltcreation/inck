3f9bf0f18b21d16fff1bdcf5d6595dd6ff9baede
Nicolas Rouvière <nicolasrouviere6@gmail.com>
2014-10-21 22:34:36 +0200

    Rapport UI/UX #76 - Modification de la vue d'un article et des articles similaires - Ajout d'un css non linké

--- src/Inck/ArticleBundle/Resources/public/js/Article/show.js
+++ src/Inck/ArticleBundle/Resources/public/js/Article/show.js
@@ -2,7 +2,7 @@ $(function() {
     $(window).scroll(function(){
         var distanceTop = $('#last').offset().top - $(window).height();
         if  ($(window).scrollTop() > distanceTop)
-            $('#slidebox').animate({'right':'0px'},300);
+            $('#slidebox').animate({'right':'20px'},300);
         else
             $('#slidebox').stop(true).animate({'right':'-430px'},100);
         var distanceProgress = ($('#last').offset().top - $(window).height());
