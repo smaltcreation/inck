097f7c08b318d63b9f19de109b55bf4247c47aa4
Samy Laumonier <159024@supinfo.com>
2014-10-22 23:31:13 +0200

    Correction d'un bug lié à la fonction scrollToElement #87

--- src/Inck/CoreBundle/Resources/public/js/global.js
+++ src/Inck/CoreBundle/Resources/public/js/global.js
@@ -1,8 +1,14 @@
-$('.btn-tooltip').tooltip();
-$('.btn-popover').popover();
-$('.carousel').carousel();
+$(document).ready(function(){
+    $('.btn-tooltip').tooltip();
+    $('.btn-popover').popover();
+    $('.carousel').carousel();
+
+    var error = $('.form-group.has-error:first');
+    if(error.length !== 0) {
+        scrollToElement(error);
+    }
+});
+
 function scrollToElement(ele) {
     $(window).scrollTop(ele.offset().top-340).scrollLeft(ele.offset().left);
 }
-
-document.onload(scrollToElement($('.form-group.has-error')));
