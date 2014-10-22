2f6dc6a0e163849c36d285992767e715f3cc0207
dridzou <lucivel_v@hotmail.com>
2014-10-22 11:34:34 +0200

    Formulaires : page de registration #82  - mise en place de la fonctionnalité sur l'ensemble du site.

--- src/Inck/CoreBundle/Resources/public/js/global.js
+++ src/Inck/CoreBundle/Resources/public/js/global.js
@@ -1,3 +1,8 @@
 $('.btn-tooltip').tooltip();
 $('.btn-popover').popover();
 $('.carousel').carousel();
+function scrollToElement(ele) {
+    $(window).scrollTop(ele.offset().top-340).scrollLeft(ele.offset().left);
+}
+
+document.onload(scrollToElement($('.form-group.has-error')));
--- src/Inck/UserBundle/Resources/public/js/Registration/register.js
+++
@@ -1,5 +0,0 @@
-function scrollToElement(ele) {
-    $(window).scrollTop(ele.offset().top-340).scrollLeft(ele.offset().left);
-}
-
-document.onload(scrollToElement($('.has-error')));
