9bf2eba70450562d606d21d55fe16c4b52fb9801
Samy Laumonier <159024@supinfo.com>
2014-11-28 01:08:24 +0100

    Redémarrage de Ratchet dans update.sh

--- app/Resources/sh/update.sh
+++ app/Resources/sh/update.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Metre à jour des vendors
-#composer self-update
-#composer update
+# Mettre à jour les sources
+git pull
+
+# Metre à jour les vendors
+composer self-update
+composer update
 
 # Supprimer le cache
 php app/console cache:clear --env=prod
@@ -23,4 +26,28 @@ if [ -z "$1" ]; then
     php app/console assetic:dump
 elif [ $1 = "--env=prod" ]; then
     php app/console assetic:dump --env=prod --no-debug
+fi
+
+# Stopper Ratchet
+echo "Stopping Ratchet..."
+id=`ps aux | grep '[p]hp -f app/console inck:ratchet:start' | awk '{print $2}'`
+
+if [ ! -z "$id" ]; then
+    echo "$(tput setaf 4)kill $id$(tput sgr0)"
+    kill $id
+else
+    echo "$(tput setaf 4)Ratchet is already stopped$(tput sgr0)"
+fi
+
+# Relancer Ratchet
+echo "Starting Ratchet..."
+php -f app/console inck:ratchet:start > /dev/null &
+
+# Vérifier l'état de Ratchet
+id=`ps aux | grep '[p]hp -f app/console inck:ratchet:start' | awk '{print $2}'`
+
+if [ ! -z "$id" ]; then
+    echo "$(tput setaf 2)Ratchet is on : pid = $id$(tput sgr0)"
+else
+    echo "$(tput setaf 1)Ratchet is off !$(tput sgr0)"
 fi
