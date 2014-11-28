c8ea60ef60f68bb9797bbe660b66e93ff379b2f4
Samy Laumonier <159024@supinfo.com>
2014-11-28 19:47:58 +0100

    Ajout de paquets manquant en prod dans le script d'installation #107

--- app/Resources/sh/install.sh
+++ app/Resources/sh/install.sh
@@ -10,16 +10,19 @@ git config core.fileMode false
 
 # Installation des paquets requis
 apt-get update
+
 # Installtion de NGINX/MYSQL/CURL
 apt-get install nginx mysql-server curl
+
 # Installation de PHP
 apt-get install php5-intl php-pear php5-memcache php5-curl php5-mysql
+
 # Installation de Composer
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
-# Installation des dépendences
-apt-get install nodejs npm zlibc memcached
 
+# Installation des dépendances
+apt-get install nodejs npm zlibc memcached libxrender1 libxext6
 npm install less@1.7.5 -g
 pecl install memcache
 
