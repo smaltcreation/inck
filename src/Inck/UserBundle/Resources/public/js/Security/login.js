c627ad1d96595fc512e1858ddcee95de399ca774
Nicolas Rouvière <nicolasrouviere6@gmail.com>
2015-01-08 15:48:20 +0100

    Ajout d'un deuxième format de vidéo pour le login sur Firefox (.ogv)

--- src/Inck/UserBundle/Resources/public/js/Security/login.js
+++ src/Inck/UserBundle/Resources/public/js/Security/login.js
@@ -1,11 +1,15 @@
 $(function() {
-    var BV = new $.BigVideo();
+    var BV = new $.BigVideo({useFlashForFirefox:false});
     BV.init();
 
     if (Modernizr.touch) {
-        BV.show('bundles/inckuser/images/drink.mp4');
+        BV.show('bundles/inckuser/images/drink.png');
     } else {
-        BV.show('bundles/inckuser/video/drink.mp4',{ambient:true});
+        BV.show([
+            { type: "video/mp4",  src: "bundles/inckuser/video/drink.mp4" },
+            { type: "video/ogg",  src: "bundles/inckuser/video/drink.ogv" }
+        ],{ambient:true});
+
         BV.getPlayer().on('durationchange',function(){
             $('#big-video-wrap').fadeIn();
         });
