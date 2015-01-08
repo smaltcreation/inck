bb857ab7275b836e14a1e20a95ee86ace5ed957b
Nicolas Rouvière <nicolasrouviere6@gmail.com>
2015-01-08 15:09:51 +0100

    Correction des tabindex du formulaire de connexion Ahout d'une transition lors du chargement de la vidéo du login

--- src/Inck/UserBundle/Resources/public/js/Security/login.js
+++ src/Inck/UserBundle/Resources/public/js/Security/login.js
@@ -1,9 +1,13 @@
 $(function() {
     var BV = new $.BigVideo();
     BV.init();
+
     if (Modernizr.touch) {
-        BV.show('video-poster.jpg');
+        BV.show('bundles/inckuser/images/drink.mp4');
     } else {
         BV.show('bundles/inckuser/video/drink.mp4',{ambient:true});
+        BV.getPlayer().on('durationchange',function(){
+            $('#big-video-wrap').fadeIn();
+        });
     }
 });
