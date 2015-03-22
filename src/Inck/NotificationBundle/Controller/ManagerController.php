b70a7d46eedebc6a97ab928f448decd3ba45b476
Samy Laumonier <159024@supinfo.com>
2015-03-22 01:01:38 +0100

    Liste de toutes les notifications dans une page avec pagination #122

--- src/Inck/NotificationBundle/Controller/ManagerController.php
+++ src/Inck/NotificationBundle/Controller/ManagerController.php
@@ -12,36 +12,63 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  */
 class ManagerController extends Controller
 {
-    /**
-     * @Route("/manager/history/{page}",
-     *     name="inck_notification_manager_history",
-     *     requirements={"page" = "\d+"},
-     *     options={"expose"=true}
-     * )
-     * @Secure(roles="ROLE_USER")
-     * @Template()
-     * @param int $page
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function historyAction($page)
+	/**
+	 * @Route("/list/{page}", name="inck_notification_manager_list", requirements={"page" = "\d+"})
+	 * @Secure(roles="ROLE_USER")
+	 * @Template()
+	 *
+	 * @param int $page
+	 * @param bool $popover
+	 *
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 * @internal param int $notificationsPerPage
+	 *
+	 */
+    public function listAction($page, $popover)
     {
         $repository = $this->getDoctrine()->getRepository('InckNotificationBundle:Notification');
         $manager = $this->get('inck_notification.manager.notification_manager');
         $paginator = $this->get('knp_paginator');
-        $renders = array();
 
         $notifications = $paginator->paginate(
             $repository->getPaginatorQuery($this->getUser()),
             $page,
-            5
+	        $popover ? 3 : 6
         );
 
         foreach ($notifications as $notification) {
-            $renders[] = $manager->render($notification);
+	        $notification->html = $manager->render($notification);
         }
 
         return array(
-            'notifications' => $renders,
+            'notifications' => $notifications,
+	        'popover'       => $popover,
         );
     }
+
+	/**
+	 * @Route("/popover", name="inck_notification_manager_popover", options={"expose"=true})
+	 * @Secure(roles="ROLE_USER")
+	 * @Template()
+	 */
+	public function popoverAction()
+	{
+		return array();
+	}
+
+	/**
+	 * @Route("/history/{page}", name="inck_notification_manager_history", defaults={"page" = "1"}, requirements={"page" = "\d+"})
+	 * @Secure(roles="ROLE_USER")
+	 * @Template()
+	 *
+	 * @param int $page
+	 *
+	 * @return array
+	 */
+	public function historyAction($page = 1)
+	{
+		return array(
+			'page' => $page,
+		);
+	}
 }
--- src/Inck/NotificationBundle/Resources/public/js/manager.js
+++ src/Inck/NotificationBundle/Resources/public/js/manager.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
-    $('.notifications-popover-toggle').popover({
+    var link = $('.notifications-popover-toggle');
+
+    link.click(function(e) {
+        e.preventDefault();
+    });
+
+    link.popover({
         html: true,
         placement: 'bottom',
-        trigger: 'hover',
         content: function() {
             return $.ajax({
-                url: Routing.generate('inck_notification_manager_history', {
-                    page: 1
-                }),
+                url: Routing.generate('inck_notification_manager_popover'),
                 dataType: 'html',
                 async: false
             }).responseText;
