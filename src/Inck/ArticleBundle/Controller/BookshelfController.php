2511774075401a4fcf6fffc61c0a3cfec7735122
Jitrixis <170535@supinfo.com>
2015-09-10 17:10:51 +0200

    Inck : Listes de lecture (#42) Correction texte erreur de propriétaire différent

--- src/Inck/ArticleBundle/Controller/BookshelfController.php
+++ src/Inck/ArticleBundle/Controller/BookshelfController.php
@@ -31,7 +31,7 @@ class BookshelfController extends Controller
     {
         if (!$bookshelf->getShare()) {
             if($this->getUser() !== $bookshelf->getUser()) {
-                throw $this->createAccessDeniedException("Vous n'avez pas le droit de supprimé cette bibliothèque !");
+                throw $this->createAccessDeniedException("Vous n'avez pas l'accès cette bibliothèque !");
             }
         }
 
